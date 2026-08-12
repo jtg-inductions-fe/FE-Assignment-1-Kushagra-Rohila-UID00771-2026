fa35d72a2befe3c75ff173b26816af32999087f3
KushagraHash <kushagra.rohila@joshtechnologygroup.com>
2026-08-12 14:26:34 +0530

    [KR_A1_02]: Improves code according to review

--- src/script/index.js
+++ src/script/index.js
@@ -1,52 +1,57 @@
-// Grab required DOM elements
-const menuToggle = document.getElementById('menuToggle');
-const categoryMenu = document.getElementById('categoriesMenu');
-const categoriesToggle = document.getElementById('categoriesToggle');
-const categoryList = document.getElementById('categoryList');
-const categoryWrapper = document.getElementById('categoryWrapper');
-const headerNavbarButton = document.querySelectorAll('.header__navbar__button');
+document.addEventListener('DOMContentLoaded', () => {
+    // Grab required DOM elements
+    const menuToggle = document.getElementById('menuToggle');
+    const categoryMenu = document.getElementById('categoriesMenu');
+    const categoriesToggle = document.getElementById('categoriesToggle');
+    const categoryList = document.getElementById('categoryList');
+    const categoryWrapper = document.getElementById('categoryWrapper');
+    const headerNavbarButton = document.querySelectorAll(
+        '.header__navbar__button',
+    );
 
-// Step 4.1: Mobile Drawer Toggle
-menuToggle.addEventListener('click', (e) => {
-    e.stopPropagation(); // Prevents instant closing from outer click event
-    categoryMenu.classList.toggle('dropdown-menu--active');
-    menuToggle.classList.toggle('color--blue');
-});
-
-// Step 4.2: Submenu Toggle (Mobile & Desktop click support)
-categoriesToggle.addEventListener('click', (e) => {
-    e.stopPropagation();
-    categoryList.classList.toggle('dropdown-category__list--active');
-    categoriesToggle.classList.toggle('color--blue');
-});
-
-headerNavbarButton.forEach((element) => {
-    element.addEventListener('mouseenter', () => {
-        element.classList.add('color--blue');
+    // Mobile Drawer Toggle
+    menuToggle.addEventListener('click', (e) => {
+        e.stopPropagation(); // Prevents instant closing from outer click event
+        categoryMenu.classList.toggle('dropdown-menu--active');
+        menuToggle.classList.toggle('color--blue');
     });
-    element.addEventListener('mouseleave', () => {
-        if (element === categoriesToggle) {
-            return;
+
+    // Submenu Toggle (Mobile & Desktop click support)
+    categoriesToggle.addEventListener('click', (e) => {
+        e.stopPropagation();
+        categoryList.classList.toggle('dropdown-category__list--active');
+        categoriesToggle.classList.toggle('color--blue');
+    });
+
+    headerNavbarButton.forEach((element) => {
+        element.addEventListener('mouseenter', () => {
+            element.classList.add('color--blue');
+        });
+        element.addEventListener('mouseleave', () => {
+            if (element === categoriesToggle) {
+                return;
+            }
+            element.classList.remove('color--blue');
+        });
+    });
+
+    //Close menus when user clicks outside
+    document.addEventListener('click', (e) => {
+        // If click is outside mobile menu, close it
+        if (!categoryMenu.contains(e.target)) {
+            categoryMenu.classList.remove('dropdown-menu--active');
+            menuToggle.classList.remov('color--blue');
         }
-        element.classList.remove('color--blue');
     });
-});
 
-// Step 4.3: Close menus when user clicks outside
-document.addEventListener('click', (e) => {
-    // If click is outside mobile menu, close it
-    if (!categoryMenu.contains(e.target)) {
-        categoryMenu.classList.remove('dropdown-menu--active');
-    }
-});
+    categoryWrapper.addEventListener('mouseenter', () => {
+        categoryList.classList.add('dropdown-category__list--active');
+        categoriesToggle.classList.add('color--blue');
+    });
 
-categoryWrapper.addEventListener('mouseenter', () => {
-    categoryList.classList.add('dropdown-category__list--active');
-    categoriesToggle.classList.add('color--blue');
-});
-
-// Triggered when the mouse cursor exits the element
-categoryWrapper.addEventListener('mouseleave', () => {
-    categoryList.classList.remove('dropdown-category__list--active');
-    categoriesToggle.classList.remove('color--blue');
+    // Triggered when the mouse cursor exits the element
+    categoryWrapper.addEventListener('mouseleave', () => {
+        categoryList.classList.remove('dropdown-category__list--active');
+        categoriesToggle.classList.remove('color--blue');
+    });
 });
